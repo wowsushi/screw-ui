b279b3a5560367c6c4cd3472aae8878f08d9541e
Betty <s9654025@gmail.com>
2021-11-23 12:18:14 +0800

    [feat] #4 implement Color Palette story

---
+++ src/components/ColorPalette/ColorPalette.stories.tsx
@@ -0,0 +1,50 @@
+import { Meta, Story } from "@storybook/react";
+import styled from "styled-components";
+
+import { DEFAULT_COLORS } from "../../theme/default-colors";
+
+export default {
+  title: "Example/ColorPalette",
+} as Meta;
+
+const StyledColorPalettes = styled.div`
+  display: flex;
+  margin: 20px 0;
+  min-width: 800px;
+`;
+
+const Box = styled.div<{ colorName: string; colorLevel: string }>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 80px;
+  background-color: ${({ theme, colorName, colorLevel }) =>
+    theme.colors[colorName][colorLevel]};
+`;
+
+const Template: Story = () => {
+  const ColorPalettes = [];
+
+  Object.keys(DEFAULT_COLORS).map((colorName) => {
+    const ColorPalette = DEFAULT_COLORS[colorName].map((_, index) => {
+      return (
+        <Box
+          key={`${colorName}-${index}`}
+          colorName={colorName}
+          colorLevel={index}
+        >{`${colorName}-${index}`}</Box>
+      );
+    });
+
+    ColorPalettes.push(
+      <StyledColorPalettes>{ColorPalette}</StyledColorPalettes>
+    );
+  });
+
+  return <>{ColorPalettes}</>;
+};
+
+export const ShowRoom = Template.bind({});
+
+ShowRoom.args = {};
