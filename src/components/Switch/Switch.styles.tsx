ccc2e26cd1116d06e894448f477c51fd21664941
Betty <s9654025@gmail.com>
2021-12-01 20:47:12 +0800

    [style] Remove unused code

--- src/components/Switch/Switch.styles.tsx
+++ src/components/Switch/Switch.styles.tsx
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import { isAccessor } from "typescript";
 
 import { SCREW_COLOR_MAPPING } from "@/theme/default-theme";
 import { ToPrivateProperty } from "@/theme/types";
